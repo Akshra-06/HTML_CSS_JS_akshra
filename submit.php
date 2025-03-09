ba544b5ca874034db328d80ce493eade1093c682
akshra-ahuja <akshraahuja06@gmail.com>
2025-03-09 15:01:52 +0530

    added a new file, made changes in the pre existing file like added checkbox,date,reset option in form

--- submit.php
+++ submit.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_POST["sub"])) {
-    // Get the 'myname' value from the POST data
+    
     $name = $_POST['myname'];
 $gender=$_POST['gender'];
 $text=$_POST['txtarea'];
-    // Display the name
+    
     echo "<h1>Form Submitted</h1>";
     echo "Your name: " . $name; 
     echo "<br>";
